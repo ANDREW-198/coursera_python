79ddb9125b27c7eff8996a221cd401938a67e9d1
Dimka-1985 <>
2021-05-06 14:17:35 +0300

    Реализовал проверку данных функции get_car_list. Отсалось написать условия для добавления Car к определенному классу и добавления его в список.

--- week_03/Cars.py
+++ week_03/Cars.py
@@ -1,5 +1,5 @@
 import os.path
-
+import csv
 
 class CarBase:
 
@@ -24,7 +24,7 @@ class Truck(CarBase):
     car_type = 'truck'
     def __init__(self, brand, photo_file_name, carrying, body_whl = '0x0x0'):
         super().__init__(brand, photo_file_name, carrying)
-        self.body_length, self.body_width, self.body_height = list(map(float,body_whl.split(sep='x')))
+        self.body_length, self.body_width, self.body_height = list(map(float, body_whl.split(sep='x')))
 
     def get_body_volume(self):
         return self.body_length * self.body_width * self.body_height
@@ -38,5 +38,22 @@ class SpecMachine(CarBase):
 
 def get_car_list(csv_filename): #FIX MI
     car_list = []
+    list_car_type = ['car', 'truck', 'spec_machine']
+    list_photo_type = ['.jpg', '.jpeg', '.png', '.gif']
+    with open(csv_filename) as csv_fd:
+        reader = csv.reader(csv_fd, delimiter=';')
+        next(reader)  # пропускаем заголовок
+        for row in reader:
+            try:
+                if row[0] in list_car_type and len(row[1]) != 0 and os.path.splitext(row[3])[1] in list_photo_type and len(row[5]) != 0:
+                    print(row)
+                else:
+                    continue
+            except IndexError:
+                continue
+
+
+
     return car_list
 
+get_car_list('coursera_week3_cars.csv')
